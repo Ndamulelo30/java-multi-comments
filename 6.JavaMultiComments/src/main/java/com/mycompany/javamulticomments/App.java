fc86d33976b7d834b15840fffa56b901c1881390
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 13:43:50 +0200

    Add files via upload

---
+++ 6.JavaMultiComments/src/main/java/com/mycompany/javamulticomments/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamulticomments;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        /*The code below will print the words Hello World
+        to the screen, and it is amazing
+        */
+        System.out.println("I am fearfully and wonderfully made");
+    }
+}
